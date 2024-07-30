1dbfd80a225aaa9d5831517ab449a3033341fe06
Kranthi Teja <kranthidirector123@gmail.com>
2024-07-30 12:24:28 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.227.161:9000/',
+  serverUrl: 'http://13.232.6.105:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login':
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'Director',
+	    'sonar.login':'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',
+	    //'sonar.login':
+	    //'sonar.password': 
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
